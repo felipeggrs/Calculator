0d4d10bbabd5c1f659d033712da2772abe423a1f
Felipe S <felipeggrs@gmail.com>
2023-03-23 17:34:13 -0300

    Add operate function

--- javascript.js
+++ javascript.js
@@ -1,3 +1,22 @@
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = "";
+
+// takes 3 arguments and works based on the operator used
+function operate(firstNumber, operator, secondNumber) {
+  if (operator === "+") {
+    return add(firstNumber, secondNumber);
+  } else if (operator === "-") {
+    return subtract(firstNumber, secondNumber);
+  } else if (operator === "*") {
+    return multiply(firstNumber, secondNumber);
+  } else if (operator === "/") {
+    return divide(firstNumber, secondNumber);
+  } else {
+    return "Invalid operation, please try again";
+  }
+}
+
 // 4 numeric functions:
 // add
 function add(...numbers) {
