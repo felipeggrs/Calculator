7b291cfb54e5562490d8aec05577f966f4adc283
Felipe S <felipeggrs@gmail.com>
2023-03-24 10:40:02 -0300

    Update buttons to display their value when clicked
    This update added some functionality to the buttons,
    such as that when they are clicked they will display
    their values above the numpad of the calculator.
    
    Additionally, the operator buttons "+-/*" now update
    the variables "firstNumber" and "operator".

--- javascript.js
+++ javascript.js
@@ -1,6 +1,70 @@
+// general variables
 let firstNumber = 0;
 let secondNumber = 0;
 let operator = "";
+const display = document.querySelector(".display");
+
+// C button
+const clearBtn = document.querySelector(".clear");
+clearBtn.addEventListener("click", () => {
+  firstNumber = 0;
+  secondNumber = 0;
+  operator = "";
+  display.textContent = "";
+});
+
+// operator buttons
+// add button
+const addBtn = document.querySelector(".addBtn");
+addBtn.addEventListener("click", () => {
+  firstNumber = display.textContent;
+  operator = "+";
+  display.textContent += addBtn.textContent;
+});
+
+// subtract button
+const subtractBtn = document.querySelector(".subtractBtn");
+subtractBtn.addEventListener("click", () => {
+  firstNumber = display.textContent;
+  operator = "-";
+  display.textContent += subtractBtn.textContent
+});
+
+// multiply button
+const multiplyBtn = document.querySelector(".multiplyBtn");
+multiplyBtn.addEventListener("click", () => {
+  firstNumber = display.textContent;
+  operator = "*";
+  display.textContent += multiplyBtn.textContent
+});
+
+// divide button
+const divideBtn = document.querySelector(".divideBtn");
+divideBtn.addEventListener("click", () => {
+  firstNumber = display.textContent;
+  operator = "/";
+  display.textContent += divideBtn.textContent
+});
+
+// equal button
+const equalBtn = document.querySelector(".equalBtn");
+equalBtn.addEventListener("click", () => {
+  display.textContent += `${equalBtn.textContent}`
+});
+
+
+// digit buttons
+const buttons = document.querySelectorAll(".btn");
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    display.textContent += button.textContent;
+  })
+})
+
+
+
+
+
 
 // takes 3 arguments and works based on the operator used
 function operate(firstNumber, operator, secondNumber) {
